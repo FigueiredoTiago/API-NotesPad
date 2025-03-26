5fb6544136c966d97968767757fa22db18dbc5aa
TFscript <developertiagof@gmail.com>
2025-03-26 16:24:30 -0300

    Test(Workflows e Testes Automatizados Atualizados Com Sucesso CI)

--- jest.config.ts
+++ jest.config.ts
@@ -3,10 +3,15 @@ import type { Config } from "@jest/types";
 const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json",
-    },
+
+  // A configuração do ts-jest deve ser movida para a seção transform
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json", // Defina outras configurações se necessário
+      },
+    ],
   },
 
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
