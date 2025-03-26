50b56b800a06431c701fd4a459f35a13aa7d55cd
TFscript <developertiagof@gmail.com>
2025-03-26 08:43:56 -0300

    Docs(criando Documentacao da Api com Swagger)

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,9 @@
 import app from "./index"; // Importa o app configurado
 import swaggerUi from "swagger-ui-express";
 import yaml from "yamljs";
-
 const port = 3000; // Defina a porta que sua API vai rodar
 
-const swaggerDocument = yaml.load("./swagger.yaml");
+const swaggerDocument = yaml.load("../swagger.yaml");
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
