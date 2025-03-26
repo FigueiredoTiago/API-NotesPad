84250e38d31bb5ea4f65ab5e04b473c8ca8681ce
TFscript <developertiagof@gmail.com>
2025-03-26 09:13:54 -0300

    update(adc url do servidor do swagger com ENV)

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,17 @@ import app from "./index"; // Importa o app configurado
 import swaggerUi from "swagger-ui-express";
 import yaml from "yamljs";
 const port = 3000; // Defina a porta que sua API vai rodar
+import dotenv from "dotenv";
+dotenv.config();
 
-const swaggerDocument = yaml.load(__dirname + '/swagger.yaml');
+const swaggerDocument = yaml.load(__dirname + "/swagger.yaml");
 
-
+swaggerDocument.servers = [
+  {
+    url: process.env.SWAGGER_SERVER_URL || "http://localhost:3000",
+    description: "Servidor de Produção",
+  },
+];
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
