8fbc44648c4a8ca641a456a0cc046eaf5e1020ea
TFscript <developertiagof@gmail.com>
2025-03-25 21:35:03 -0300

    fixx(ordem de limpeza do bd alterada, evitando bug de exclusao de dependentes

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,12 +1,12 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 // Limpar o banco de dados antes de cada teste
 beforeEach(async () => {
   // Limpar as tabelas 'user' e 'note' (adicione outras tabelas conforme necessário)
-  await prisma.user.deleteMany({});
   await prisma.note.deleteMany({});
+  await prisma.user.deleteMany({});
 });
 
 // Fechar a conexão com o Prisma após todos os testes
